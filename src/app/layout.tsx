c8f0180da19b1ba354d2b81f9e4d50470940388f
KevDev1437 <projectskevin834@gmail.com>
2025-09-24 12:28:34 +0200

    Add body.png as full background image for the entire body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,14 @@ export default function RootLayout({
   return (
     <html lang="fr" className="light">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50 text-gray-900`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased text-gray-900`}
+        style={{
+          backgroundImage: 'url(/body.png)',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          backgroundAttachment: 'fixed'
+        }}
       >
         <Header />
         <main className="pt-0">
