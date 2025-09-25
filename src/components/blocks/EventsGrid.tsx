64d4d206daf4dbb941006b01f804fc26b25fffe4
KevDev1437 <projectskevin834@gmail.com>
2025-09-25 10:52:26 +0200

    ok

--- src/components/blocks/EventsGrid.tsx
+++ src/components/blocks/EventsGrid.tsx
@@ -70,7 +70,7 @@ export default async function EventsGrid() {
       
       {workshops && workshops.length > 0 ? (
         <>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 justify-items-center">
             {workshops.map((workshop) => (
               <EventCard 
                 key={workshop.id} 
